374e0f30b5d85b4bbdf89b14b084ebf7dca8ff51
sian-tku <103718982+sian-tku@users.noreply.github.com>
2022-10-19 15:02:45 +0800

    w06

---
+++ w06/js/demo.js
@@ -0,0 +1,13 @@
+function showpage(index) {
+  switch (index) {
+    case 1:
+      p.innerHTML = `<iframe src="https://sp.booking.com/index.html?aid=1538014&label=zhtw-edge-ntp-topsites-curate-ana" wid />`;
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+  }
+}
